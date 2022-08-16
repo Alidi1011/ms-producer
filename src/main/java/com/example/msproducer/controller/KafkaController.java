659958f64621c80cb91620b182cd23e582acfb4d
alisson <arteaga10alisson@gmail.com>
2022-08-16 17:03:30 -0500

    add payment wallet topic

--- src/main/java/com/example/msproducer/controller/KafkaController.java
+++ src/main/java/com/example/msproducer/controller/KafkaController.java
@@ -2,6 +2,7 @@ package com.example.msproducer.controller;
 
 import com.example.msproducer.dto.PaymentDto;
 import com.example.msproducer.producer.PaymentTransactionProducer;
+import com.example.msproducer.producer.PaymentWalletProducer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/kafka")
 public class KafkaController {
   private final PaymentTransactionProducer paymentTransactionProducer;
+  private final PaymentWalletProducer paymentWalletProducer;
 
   @PostMapping("/sendPayment")
   public ResponseEntity<String> sendMessage(@RequestBody PaymentDto paymentDto) {
     log.info("Sending message {}", paymentDto);
     paymentTransactionProducer.sendMessage(paymentDto);
+    return ResponseEntity.ok("Message sent");
+  }
+
+  @PostMapping("/sendPaymentWallet")
+  public ResponseEntity<String> sendMessageWallet(@RequestBody PaymentDto paymentDto) {
+    log.info("Sending message {}", paymentDto);
+    paymentWalletProducer.sendMessage(paymentDto);
     return ResponseEntity.ok("Message sent");
   }
 }
