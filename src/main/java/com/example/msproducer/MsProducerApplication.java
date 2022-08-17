19f037d0d6a54507e06837caa750cccf6414f301
Christian Dionisio <christiandionisio9432@gmail.com>
2022-08-17 16:15:06 -0500

    add eureka client

--- src/main/java/com/example/msproducer/MsProducerApplication.java
+++ src/main/java/com/example/msproducer/MsProducerApplication.java
@@ -2,8 +2,10 @@ package com.example.msproducer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsProducerApplication {
 
   public static void main(String[] args) {
